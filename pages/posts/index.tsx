93b37e0025f1836a47ddf3f6bac785eee8908644
노수아 <nosua@ip-192-168-200-124.ap-northeast-2.compute.internal>
2023-06-26 23:41:51 +0900

    feat: hide posts

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -3,18 +3,36 @@ import { getSortedPostsData } from '../../lib/post';
 import { List, Seo } from '../../components';
 import { GetStaticProps } from 'next';
 import styles from '../../styles/Posts.module.scss';
+import { useMemo } from 'react';
 
 interface PostsProps {
   allPostData: Array<PostType>;
 }
 
+const HIDE_POSTS = [
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '10',
+    '12',
+]
+
 const Posts = ({ allPostData }: PostsProps) => {
+const posts = useMemo(()=>allPostData.filter(({id}) =>
+       !HIDE_POSTS.includes(id)
+    ,[]),[allPostData])
+
   return (
     <>
       <Seo title="posts" />
       <section className={styles.root}>
         <ul>
-          {allPostData.map((e) => {
+          {posts.map((e) => {
             return <List key={e.id} {...e} />;
           })}
         </ul>
